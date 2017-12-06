7282c9821032947f54a9f94edea81f0ad17da632
Pierre Dalbkermeyer <pierre.dalbkermeyer@adviqo.com>
2017-12-06 10:47:02 +0100

    Added pagination

--- cms/src/main/js/components/endpoint_show.js
+++ cms/src/main/js/components/endpoint_show.js
@@ -10,9 +10,20 @@ class EndpointShow extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pageSize: 1
+            pageSize: 10,
+            firstAvailable: 'disabled',
+            prevAvailable: 'disabled',
+            nextAvailable: 'disabled',
+            lastAvailable: 'disabled',
+            totalElements: 0,
+            totalPages: 0,
+            currentPage: 1
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleFirst = this.handleFirst.bind(this);
+        this.handleLast = this.handleLast.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+        this.handlePrev = this.handlePrev.bind(this);
 
     }
 
@@ -23,6 +34,21 @@ class EndpointShow extends Component {
             this.props.fetchEndpoint(url.replace(/profile\//, ''), this.state.pageSize);
         }
     }
+
+    componentWillReceiveProps(nextProps){
+        if (!_.isEmpty(nextProps.endpoint)){
+            this.setState({
+                firstAvailable: (_.isEmpty(nextProps.endpoint.first))? 'disabled':'',
+                prevAvailable: (_.isEmpty(nextProps.endpoint.prev))? 'disabled':'',
+                nextAvailable: (_.isEmpty(nextProps.endpoint.next))? 'disabled':'',
+                lastAvailable: (_.isEmpty(nextProps.endpoint.last))? 'disabled':'',
+                totalElements: nextProps.endpoint.page.totalElements,
+                totalPages: nextProps.endpoint.page.totalPages,
+                currentPage: nextProps.endpoint.page.number + 1
+            });
+        }
+    }
+
 
     handleChange(e) {
         e.preventDefault();
@@ -39,9 +65,39 @@ class EndpointShow extends Component {
         this.setState({pageSize});
     }
 
+    // updateDisabledState() {
+    //         nextAvailable: true
+    //     var prevAvailable = (_.isEmpty(this.props.endpoint.prev))? 'disabled':'';
+    // }
+
+    handleFirst(e) {
+        e.preventDefault();
+        this.props.fetchEndpoint(this.props.endpoint.first.href);
+
+    }
+
+    handleLast(e) {
+        e.preventDefault();
+        this.props.fetchEndpoint(this.props.endpoint.last.href);
+    }
+
+    handleNext(e) {
+        e.preventDefault();
+        if (!_.isEmpty(this.props.endpoint.next.href)) {
+            this.props.fetchEndpoint(this.props.endpoint.next.href);
+        }
+    }
+
+    handlePrev(e) {
+        e.preventDefault();
+        if (!_.isEmpty(this.props.endpoint.prev.href)) {
+            this.props.fetchEndpoint(this.props.endpoint.prev.href);
+        }
+    }
+
     renderEndpoint() {
         var that = this;
-        return _.map(this.props.endpoint, function(value, key) {
+        return _.map(this.props.endpoint.data, function(value, key) {
             if (!_.isEmpty(value)) {
                 var names = _.map(that.props.endpoint_meta, function(v, k) {
                     return (
@@ -71,7 +127,7 @@ class EndpointShow extends Component {
     renderTable() {
         if (!_.isEmpty(this.props.endpoint_meta)) {
             return (
-                <table className="table table-hover table-bordered">
+                <table className="table table-hover table-bordered table-striped">
                     <thead>
                     {this.renderEndpointHead()}
                     </thead>
@@ -93,14 +149,41 @@ class EndpointShow extends Component {
                     <Link to={"/services/" + this.props.match.params.id} className="btn btn-secondary">Back</Link>
                 </div>
                 <h3>{"Endoint: " + this.props.match.params.endpoint}</h3>
-                {/*<input ref="pageSize" defaultValue={this.state.pageSize} onInput={this.handleChange} />*/}
-                <select value={this.state.pageSize} onChange={this.handleChange}>
-                    <option value="1">1</option>
-                    <option value="10">10</option>
-                    <option value="20">20</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                </select>
+                <div className="d-flex justify-content-between align-items-start">
+                    <div className="p-4">
+                        <select className="form-control" value={this.state.pageSize} onChange={this.handleChange}>
+                            <option value="1">1</option>
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select>
+                    </div>
+                    <div className="p-4">
+                        <nav aria-label="Page navigation example">
+                            <ul className="pagination">
+                                <li className="page-item">
+                                    <a className="page-link" href="" onClick={this.handleFirst} aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                        <span className="sr-only">Previous</span>
+                                    </a>
+                                </li>
+                                <li className={"page-item " + this.state.prevAvailable}><a className="page-link" onClick={this.handlePrev} href="">&lsaquo;</a></li>
+                                <li className={"page-item " + this.state.nextAvailable}><a className="page-link" onClick={this.handleNext} href="">&rsaquo;</a></li>
+                                <li className="page-item">
+                                    <a className="page-link" href="" onClick={this.handleLast} aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                        <span className="sr-only">Next</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="p-4">
+                        <span className="badge badge-pill badge-info">{(this.state.currentPage) + " of " + this.state.totalPages + " pages"}</span>
+                        <span className="badge badge-pill badge-info">{this.state.totalElements + " entries"}</span>
+                    </div>
+                </div>
                 {this.renderTable()}
             </div>
         )
--- cms/src/main/js/reducers/reducer_endpoint.js
+++ cms/src/main/js/reducers/reducer_endpoint.js
@@ -9,7 +9,16 @@ export default function(state = {}, action) {
             // newState[service.id] = post;
             // return newState;
             // return { ...state, [action.payload.data.id]: action.payload.data };
-            return action.payload.entity._embedded[Object.keys(action.payload.entity._embedded)[0]];
+            return {
+                data: action.payload.entity._embedded[Object.keys(action.payload.entity._embedded)[0]],
+                page: action.payload.entity.page,
+                first: action.payload.entity._links.first,
+                last: action.payload.entity._links.last,
+                next: action.payload.entity._links.next,
+                prev: action.payload.entity._links.prev,
+                profile: action.payload.entity._links.profile
+            };
+            // return action.payload.entity._embedded[Object.keys(action.payload.entity._embedded)[0]];
         default:
             return state;
     }
